4064a085bf45ca03ceddc5d361a54d09ef658f62
lp1235_win <huanhuan1235@naver.com>
2024-01-02 00:42:43 +0900

    Implement article controllers

--- Chapter07/node-simple-board-skeleton/src/controller/ctrl.js
+++ Chapter07/node-simple-board-skeleton/src/controller/ctrl.js
@@ -7,6 +7,8 @@
 //     }
 // }
 
+const { ArticleDAO } = require("../DAO");
+
 const indexPage = async(req, res, next) => {
     try {
         const {user} = req.session;
@@ -16,4 +18,44 @@ const indexPage = async(req, res, next) => {
     }
 };
 
-module.exports = {indexPage};
+// GET /articles/page/:page(\\d+)
+const listArticles = async (req, res, next) => {
+    try {
+        const {page} = req.params;
+        const {user} = req.sessions;
+        const pageNum = parseInt(page, 10);
+        if (pageNum <= 0) throw new Error('BAD_REQUEST');
+
+        const ARTICLES_PER_PAGE = 10;
+        const startIndex = (pageNum - 1) * ARTICLES_PER_PAGE;
+
+        const articles = await ArticleDAO.getList(startIndex, ARTICLES_PER_PAGE);
+        const articleCount = await ArticleDAO.getTotalCount();
+        const pageCount = Math.ceil(articleCount / ARTICLES_PER_PAGE);
+
+        return res.render('articles/index.pug', {
+            user,
+            articles,
+            page: pageNum,
+            hasPrev: pageNum > 1,
+            hasNext: pageNum < pageCount,
+        });
+    } catch (err) {
+        return next(err);
+    }
+};
+
+// GET /articles
+const latestArticles = async (req, res, next) => {
+    try {
+        return res.redirect('/articles/page/1');
+    } catch (err) {
+        return next(err);
+    }
+};
+
+module.exports = {
+    indexPage,
+    listArticles,
+    latestArticles,
+};
