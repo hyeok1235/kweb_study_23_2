40366ee70d2121e18e71b66c1a8a8080513628ad
lp1235_win <huanhuan1235@naver.com>
2024-01-01 16:19:39 +0900

    IMPLEMENT user DAO

---
+++ Chapter07/node-simple-board-skeleton/src/DAO/user.js
@@ -0,0 +1,18 @@
+const {runQuery} = require("../lib/database");
+
+const getByUsername = async (username) => {
+    const sql = "SELECT id, password, display_name AS displayName, is_active AS isActive, is_staff AS isStaff "
+    + "FROM users WHERE username = ?";
+    const result = await runQuery(sql, [username]);
+    return result[0];
+};
+
+const create = async (username, password, displayName) => {
+    const sql = "INSERT INTO users (username, password, display_name) VALUES (?, ?, ?)";
+    await runQuery(sql, [username, password, displayName]);
+};
+
+module.exports = {
+    getByUsername,
+    create,
+};
